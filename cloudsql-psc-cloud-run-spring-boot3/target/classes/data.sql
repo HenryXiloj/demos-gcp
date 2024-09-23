4138350de8bfbd1eb7071f97634f3d646c21e869
Henry <henry.xh2@gmail.com>
2024-09-22 19:37:17 -0600

    Feat: Updating Tf PSC, Spring Boot 3 and Adding Python 3.12

--- cloudsql-psc-cloud-run-spring-boot3/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
--- Inserting data into table3 (Private Service Connect (PSC))
-INSERT INTO table3 (product, price) VALUES
-('Phone', 599.99),
-('Laptop', 1299.99),
-('Tablet', 349.99),
-('Smartwatch', 199.99);
--- cloudsql-psc-cloud-run-spring-boot3/target/classes/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS table3 (
-    id SERIAL PRIMARY KEY,
-    product VARCHAR(50),
-    price DECIMAL(10, 2)
-);
