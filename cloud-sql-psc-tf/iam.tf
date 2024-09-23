484860684239fb6c3ae3f7e9a96fbbcbd57860c3
Henry <henry.xh2@gmail.com>
2024-09-23 08:48:38 -0600

    Update iam.tf

--- cloud-sql-psc-tf/iam.tf
+++ cloud-sql-psc-tf/iam.tf
@@ -8,7 +8,8 @@ resource "google_project_iam_member" "member-role" {
     "roles/cloudsql.admin",
     "roles/secretmanager.secretAccessor",
     "roles/secretmanager.secretVersionManager",
-    "roles/vpcaccess.serviceAgent"
+    "roles/vpcaccess.serviceAgent",
+    "roles/run.developer"
   ])
   role    = each.key
   project = var.project_id
