0182998bce117b493a55480312cd7c94d82e8311
Henry <henry.xh2@gmail.com>
2025-02-18 16:59:10 -0600

    first commit pubsub

---
+++ pubsub/demo-pub-sub-api/src/main/java/com/henry/demo/pubsub/DemoPubSubApiApplication.java
@@ -0,0 +1,13 @@
+package com.henry.demo.pubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPubSubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPubSubApiApplication.class, args);
+	}
+
+}
