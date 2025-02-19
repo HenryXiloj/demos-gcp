6bcce444a87d0a28235d8532a37f51d3463306e6
Henry <henry.xh2@gmail.com>
2025-02-18 18:48:12 -0600

    first commit pubsub

--- pubsub/terraform/variable.tf
+++ pubsub/terraform/variable.tf
@@ -17,20 +17,3 @@ variable "sec_region" {
 variable "sec_zone" {
   default = ""
 }
-
-variable "shared_secret" {
-  default = ""
-}
-
-variable "private_google_access_ips" {
-  type    = list(string)
-  default = []
-}
-
-variable "destination_range_in_peer_first_GCP_project" {
-  default = ""
-}
-
-variable "static_peer_firt_GCP_project_IP" {
-  default = ""
-}
